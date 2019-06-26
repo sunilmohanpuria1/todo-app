3892926e85de0830d038ffe852bd92e75708fdc5
sunilmohanpuria1 <sunil.k@quantinsti.com>
2019-06-26 18:14:32 +0530

    sequalize.sync removed

--- todo-app/myapp.js
+++ todo-app/myapp.js
@@ -7,11 +7,10 @@ app.set('view engine', 'ejs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//app.use(express.urlencoded());
+
 
 app.get('/todos/:id', function (req, res) {
     console.log('Request Id:', req.params.id);
-    sequelize.sync().then(function (){
         news.findAll({
             where:{
               id:req.params.id
@@ -19,18 +18,14 @@ app.get('/todos/:id', function (req, res) {
           }).then(data => {
             console.log("news data:", JSON.stringify(data));
             res.send(data);
-          });
-        });   
+          });   
   });
 
 app.put('/todos/:id',function(req,res){
-    sequelize.sync().then(function(){
-        let id= req.params.id;
-        let newsTitle= req.body.Title;
         news.update({
             newsTitle:req.body.Title
             },{
-                where:{id:id}
+                where:{id:req.params.id}
             }
         ).then((data)=>{
             news.findAll({
@@ -42,7 +37,6 @@ app.put('/todos/:id',function(req,res){
                 res.send(temp);
               }); 
         });
-    });
 });
 
 app.get('/', (req, res) => {
@@ -54,41 +48,26 @@ app.get('/add', (req, res) => {
     })
 
 app.get('/todos',(req,res)=>{
-    sequelize.sync().then(function (){
         news.findAll().then(data => {
             console.log("new data:", JSON.stringify(data));
             res.send(data);
           });
-        });
     });
 
-
 app.post('/todos',(req,res)=>{
-    sequelize.sync().then(function (){
-        console.log("req.body\n");
         console.log(req.body);
-        let Discription= req.body.Discription;
-        let newsTitle= req.body.Title;
-        //console.log(newsTitle);
-        news.create({newsTitle: newsTitle,Discription:Discription}).then(temp => {
+        news.create({newsTitle: req.body.Title,Discription:req.body.Discription}).then(temp => {
             console.log("Inserted Key", temp.ID);
             res.send(temp);
             });
-    });
 });
-
-        
+  
 app.post('/add/task', (req, res) => {
-   
-        let Discription= req.body.Discription;
-        let newsTitle= req.body.Title;
-        console.log(Discription,newsTitle);
+        news.create({newsTitle: nreq.body.Title,Discription:req.body.Discription}).then(temp => {
+            console.log("Inserted Key", temp.ID);
+            });
+    res.send('News Added');})   
 
-        sequelize.sync().then(function (){
-            news.create({newsTitle: newsTitle,Discription:Discription}).then(temp => {
-                console.log("Inserted Key", temp.ID);
-                });
-            })
-    res.send('News Added');})
-        
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+sequelize.sync().then(function(){
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
